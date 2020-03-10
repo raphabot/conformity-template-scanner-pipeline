0184ca6d452d2b47620d47a19676daf3e1ffc37a
Raphael Bottino <raphabot@gmail.com>
2020-03-10 13:49:51 -0500

    adding the scanning

--- scan.js
+++ scan.js
@@ -1,2 +1,41 @@
+"use strict";
 
-console.log("test");
+const fs = require('fs')
+const { promisify } = require('util')
+const readFile = promisify(fs.readFile)
+const CloudConformity = require("cloud-conformity");
+
+const scan = async (ccEndpoint, ccApiKey) => {
+  const cc = new CloudConformity.CloudConformity(ccEndpoint, ccApiKey);
+  const template = await readFile('s3.template.yaml', 'utf8');
+  const result = await cc.scanACloudFormationTemplateAndReturAsArrays(template);
+  return result.failure;
+};
+
+const failOnFailure = (failures, acceptedQty) => {
+  if (failures.length > acceptedQty){
+    return true;
+  }
+  return false;
+};
+
+const region = process.env.cc_region;
+const apikey = process.env.cc_apikey;
+const acceptedQty = process.env. acceptedQty;
+
+scan(region, apikey)
+  .then(res => {
+    console.log(JSON.stringify(res, null, 2));
+    console.log("Failures Quantity: ", res.length);
+    return failOnFailure(res, acceptedQty);
+  })
+  .then(res => {
+    console.log(res);
+    if (res){
+      process.exit(1);
+    }
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+  });
