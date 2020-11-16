8ce56df5317703ddc538de63d542384722526527
Raphael Bottino <raphabot@gmail.com>
2020-11-16 13:54:45 -0600

    bump

--- scan.js
+++ scan.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 "use strict";
 
 const fs = require('fs');
@@ -9,8 +10,10 @@ const scan = async (templatePath, ccEndpoint, ccApiKey) => {
   const cc = new CloudConformity.CloudConformity(ccEndpoint, ccApiKey);
   const template = await readFile(templatePath, 'utf8');
   const result = await cc.scanACloudFormationTemplateAndReturAsArrays(template);
+  const messages = [];
   // console.log(JSON.stringify(result.failure, null, 2));
   const results = result.failure.reduce((total, result) => {
+    messages.push(`Risk: ${result.attributes['risk-level']} \tReason: ${result.attributes.message}`);
     if (result.attributes['risk-level'] === 'EXTREME'){
       total.extreme +=1;
     } else if (result.attributes['risk-level'] === 'VERY_HIGH') {
@@ -28,11 +31,12 @@ const scan = async (templatePath, ccEndpoint, ccApiKey) => {
     veryHigh: 0,
     high: 0,
     medium: 0,
-    low: 0
+    low: 0,
   });
   return {
     detections: result.failure,
-    results: results
+    results: results,
+    messages: messages
   };
 };
 
@@ -53,21 +57,29 @@ const acceptedResults = {
   medium: process.env.maxMedium? process.env.maxMedium: Number.MAX_SAFE_INTEGER,
   low: process.env.maxLow? process.env.maxLow : Number.MAX_SAFE_INTEGER
 };
+const outputResults = process.env.cc_output_results? true : false;
 
-scan(templatePath, region, apikey)
+scan(templatePath, region, apikey, outputResults)
   .then(res => {
     // console.log(JSON.stringify(res.results, null, 2));
     //console.log(JSON.stringify(res, null, 2));
     console.log(`Failures found: ${JSON.stringify(res.results, null, 2)}`);
+    console.log('\n');
     console.log(`Quantity of failures allowed: ${JSON.stringify(acceptedResults, null, 2)}`);
+    if (outputResults && res.messages){
+      console.log('\n');
+      console.log('Results:\n');
+      console.log(res.messages.join('\n'));
+    }
+    console.log('\n');
     return failOnFailure(res.results, acceptedResults);
   })
   .then(res => {
     if (res){
-      console.log("Too much failures!");
+      console.log("Security and/or misconfiguration issue(s) found in template.");
       process.exit(1);
     }
-    console.log("Less failures than allowed, so let's deploy it!");
+    console.log("Template passes configured checks.");
     process.exit(0);
   })
   .catch(err => {
